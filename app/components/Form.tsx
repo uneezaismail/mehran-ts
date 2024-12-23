03f7d00e846d198cb498240c8f2ead4b2e5f63b5
uneezaismail <ismailrahet@gmail.com>
2024-12-23 15:32:57 +0500

    Update Form.tsx

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -34,7 +34,7 @@ const RegistrationForm = () => {
   // Generate PDF
   const generatePDF = async () => {
     if (formRef.current) {
-      const canvas = await html2canvas(formRef.current, { scale: 2 });
+      const canvas = await html2canvas(formRef.current, { scale: 1 });
       const imgData = canvas.toDataURL("image/png");
   
       const pdf = new jsPDF("p", "mm", "a4");
@@ -202,79 +202,140 @@ const RegistrationForm = () => {
       </button>
 
       {/* Hidden Rendered Form for PDF */}
-      <div ref={formRef} style={{ display: isPopupVisible ? "block" : "none" }}>
-        <div className="p-8 border space-y-10 rounded shadow-md w-[800px]">
-          {/* Form content */}
-          <div className="flex justify-between items-center mb-10">
-            <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128}/>
-            <div className="flex flex-col items-center gap-3">
-              <h1 className="text-2xl font-serif text-center underline mt-10">
-                MEHRAN TESTING SERVICE NAUKOT
-              </h1>
-              <h2 className="text-2xl font-bold text-center">
-                2 <sup>ND </sup> SELF ASSESSMENT TEST
-              </h2>
-              <h3 className="text-2xl font-bold text-center underline">
-                REGISTRATION FORM
-              </h3>
-            </div>
-            <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128}/>
-          </div>
+  
+  <div ref={formRef}  className="border-2 border-black p-8  space-y-10 rounded bg-white w-[800px]">
+    {/* Form content */}
+    <div className="flex justify-between items-center mb-10">
+      <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128} />
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="text-2xl font-serif text-center underline mt-10">
+          MEHRAN TESTING SERVICE NAUKOT
+        </h1>
+        <h2 className="text-2xl font-bold text-center">
+          2 <sup>ND </sup> SELF ASSESSMENT TEST
+        </h2>
+        <h3 className="text-2xl font-bold text-center underline">
+          REGISTRATION FORM
+        </h3>
+      </div>
+      <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128} />
+    </div>
 
-          {/* Form data */}
-          <div className="space-y-4">
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Name: </span><p className="border-b-2 border-b-black p-2  w-[70%] font-medium">{formData.name}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Father&apos;s Name: </span><p className="border-b-2 border-b-black p-2  w-[70%] font-medium"> {formData.fatherName}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Gender: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.gender}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Caste: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.caste}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Class: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.class}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">School: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.school}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Contact No: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.contactNo}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">Address: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.address}</p></div>
-            <div className="flex items-center gap-4 "><span className="font-bold text-xl">District: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.district}</p></div>
-          </div>
+    {/* Form data */}
+    <div className="space-y-4">
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Name: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.name}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Father&apos;s Name: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.fatherName}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Gender: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.gender}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Caste: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.caste}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Class: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.class}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">School: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.school}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Contact No: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.contactNo}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">Address: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.address}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <span className="font-bold text-xl">District: </span>
+        <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.district}</p>
+      </div>
+    </div>
 
-          <div className="flex justify-end p-8 font-bold  border-b-4 border-dashed border-b-black">
-            <p className="border-t-2 border-t-black p-2 text-xl">Student Sign</p>
-          </div>
+    <div className="flex justify-end p-8 font-bold border-b-4 border-dashed border-b-black">
+      <p className="border-t-2 border-t-black p-2 text-xl">Student Sign</p>
+    </div>
 
-          <div className="flex justify-between items-center mb-10">
-          <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128}/>
-            <div className="flex flex-col items-center gap-4">
-                <span className="bg-black px-10 py-2 text-white rounded-md font-medium">STUDENT SLIP</span>
-            <h1 className="text-2xl font-serif text-center mb-4">
-              MEHRAN TESTING SERVICE NAUKOT
-            </h1>
-            <h2 className="text-xl font-bold  text-center">
-            2 <sup>ND </sup> SELF ASSESSMENT TEST
-          </h2>        
-          </div>
-          <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128}/>
-          </div>
-          <div className="space-y-4">
-          <div className="flex items-center justify-between w-full"><div className="flex items-center gap-4 w-[50%]"><span className="font-medium text-xl">Name: </span><p className="border-b-2 border-b-black p-2  w-[70%] font-medium">{formData.name}</p></div>
-            <div className="flex items-center gap-4 w-[50%]"><span className="font-medium text-xl">F.Name: </span><p className="border-b-2 border-b-black p-2  w-[70%] font-medium"> {formData.fatherName}</p></div></div>  
-          
-            <div className="flex items-center justify-between w-full">  <div className="flex items-center gap-4  w-[50%]"><span className="font-medium text-xl">Class: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.class}</p></div>
-            <div className="flex items-center gap-4 w-[50%]"><span className="font-medium text-xl">School: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.school}</p></div> 
-            </div>  
-            
-            <div className="flex items-center justify-between w-full"> <div className="flex items-center gap-4 w-[50%]"><span className="font-medium text-xl">Gender: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.gender}</p></div>
-            <div className="flex items-center gap-4 w-[50%]"><span className="font-medium text-xl">Caste: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.caste}</p></div></div>
-           
-            <div className="flex items-center justify-between w-full"> <div className="flex items-center w-[50%] gap-4 "><span className="font-medium text-xl">Contact No: </span><p className="border-b-2 border-b-black p-2 w-[60%] font-medium"> {formData.contactNo}</p></div>
-            <div className="flex items-center gap-4 w-[50%] "><span className="font-medium text-xl">Address: </span><p className="border-b-2 border-b-black p-2 w-[70%] font-medium"> {formData.address}</p></div></div>
-            
-            <div className="flex items-center gap-4"><p className="border-b-2 border-b-black py-2 w-full font-medium"></p></div>
-            <p>Contact No. <Link href={"tel:03342492847"}>03342492847</Link> </p>
-          </div>
+    <div className="flex justify-between items-center mb-10">
+      <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128} />
+      <div className="flex flex-col items-center gap-4">
+        <span className="bg-black px-10 py-2 text-white rounded-md font-medium">STUDENT SLIP</span>
+        <h1 className="text-2xl font-serif text-center mb-4">
+          MEHRAN TESTING SERVICE NAUKOT
+        </h1>
+        <h2 className="text-xl font-bold text-center">
+          2 <sup>ND </sup> SELF ASSESSMENT TEST
+        </h2>
+      </div>
+      <Image src="/mehran-logo.jpg" alt="Logo" width={128} height={128} />
+    </div>
 
-          <div className="flex justify-between p-8 font-bold">
-            <p className="border-t-2 border-t-black p-2 text-xl">Sign of Controller</p>
-            <p className="border-t-2 border-t-black p-2 text-xl">Student Sign</p>
-          </div>
+    <div className="space-y-4">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">Name: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.name}</p>
         </div>
-      </div>     
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">F.Name: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.fatherName}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">Class: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.class}</p>
+        </div>
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">School: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.school}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">Gender: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.gender}</p>
+        </div>
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">Caste: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.caste}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center w-[50%] gap-4">
+          <span className="font-medium text-xl">Contact No: </span>
+          <p className="border-b-2 border-b-black p-2 w-[60%] font-medium">{formData.contactNo}</p>
+        </div>
+        <div className="flex items-center gap-4 w-[50%]">
+          <span className="font-medium text-xl">Address: </span>
+          <p className="border-b-2 border-b-black p-2 w-[70%] font-medium">{formData.address}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <p className="border-b-2 border-b-black py-2 w-full font-medium"></p>
+      </div>
+      <p>Contact No. <Link href={"tel:03342492847"}>03342492847</Link></p>
+    </div>
+
+    <div className="flex justify-between p-8 font-bold">
+      <p className="border-t-2 border-t-black p-2 text-xl">Sign of Controller</p>
+      <p className="border-t-2 border-t-black p-2 text-xl">Student Sign</p>
+    </div>
+  </div>
+
      
 
       {/* Popup */}
